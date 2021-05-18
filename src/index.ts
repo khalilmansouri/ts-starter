d73742daa4a8cb3bb8be8ebd1004220726a05b6c
Khalil Mansouri <khalil_mansouri@hotmail.fr>
2021-05-18 15:25:36 +0100

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,12 @@ let postService: PostService;
   await postRepo.init()
 
   postService = new PostService(postRepo);
-
-  console.log(await postService.find({}))
   postController = new PostController(new PostService(new MongodbRepo()))
-  // console.log({ postService, postController })
 
   await new Promise(r => setTimeout(r, 2000))
 
+
   let http = new ExpressAPP()
-  // console.log(Object.keys(postController.postService))
   http.GET("/", postController.find)
   http.POST("/", postController.create)
   http.listen(5000)
